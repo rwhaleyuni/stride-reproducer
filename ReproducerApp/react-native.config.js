00c9297c3ed066b05caad75ad208a1c4187faf5b
John Bodenhamer <john_bodenhamer@unigroup.com>
2025-03-18 11:29:53 -0500

    iOS failure

---
+++ ReproducerApp/react-native.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  dependencies: {
+    '@qdivision/react-native-pdf-view': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    '@qdivision/react-native-signature-capture': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'react-native-fs': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    // 'react-native-gesture-handler': {
+    //   platforms: {
+    //     android: null,
+    //     ios: null,
+    //   },
+    // },
+    // 'react-native-blob-util': {
+    //   platforms: {
+    //     android: null,
+    //     ios: null,
+    //   },
+    // },
+    '@react-native-community/netinfo': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'react-native-vector-icons': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
+};
